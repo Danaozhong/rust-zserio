9dd1f73cf191020ac6f36c5af4c0e95dd39fa9a7
Clemens Zangl <clemens.zangl@woven-planet.global>
2024-07-07 14:55:02 +0900

    correct invalid unit test

--- tests/round-trip-tests/src/expr_numbits_test.rs
+++ tests/round-trip-tests/src/expr_numbits_test.rs
@@ -5,14 +5,21 @@ use rust_zserio::ztype::ZserioPackableObject;
 /// The function get_num_bits() is defined as `return numbits(u16Value) + numbits(8);`.
 pub fn test_expr_numbits() {
     let mut test_struct = ExpressionNumbitsTest::new();
-    assert_eq!(test_struct.get_num_bits(), 4);
+    assert_eq!(test_struct.get_num_bits(), 3);
 
     test_struct.u_16_value = 1;
-    assert_eq!(test_struct.get_num_bits(), 5);
+    assert_eq!(test_struct.get_num_bits(), 4);
 
     test_struct.u_16_value = 7;
-    assert_eq!(test_struct.get_num_bits(), 7);
+    assert_eq!(test_struct.get_num_bits(), 6);
 
+    // as per
+    // https://zserio.org/doc/ZserioLanguageOverview.html
+    // numbits(8) + numbits(8) =
+    // 3 + 3
     test_struct.u_16_value = 8;
-    assert_eq!(test_struct.get_num_bits(), 8);
+    assert_eq!(test_struct.get_num_bits(), 6);
+
+    test_struct.u_16_value = 9;
+    assert_eq!(test_struct.get_num_bits(), 7);
 }
