30dc4eb48bd12ac6b07392351bae3b5ba8c4ec12
Wichert Akkerman <wichert@wiggy.net>
2024-08-07 22:42:23 +0200

    Setup a basic logging backend
    This will make it possible to swap replace println-based output to using
    logging with log levels, so the reduce can select how verbose
    rust-zserio should be.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use clap::Parser;
+use log::LevelFilter;
 use rust_zserio::internal::generator::model::generate_model;
 use rust_zserio::internal::model::Model;
+use simple_logger::SimpleLogger;
 use std::path::Path;
 
 /// zserio generator for rust.
 #[derive(Parser, Debug)]
 #[command(version = None)]
 struct Args {
+    /// Output verbosity
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+
     /// Directory where to generate the zserio interface files.
     #[arg(short, long)]
     out: String,
@@ -21,6 +27,17 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+
+    SimpleLogger::new()
+        .with_level(match args.verbose {
+            0 => LevelFilter::Warn,
+            1 => LevelFilter::Info,
+            2 => LevelFilter::Debug,
+            3.. => LevelFilter::Trace,
+        })
+        .init()
+        .expect("can not initialize logger");
+
     // Load the zserio files (*.zs) from the filesystem.
     let mut model = Model::from_filesystem(Path::new(args.zserio_path.as_str()));
 
