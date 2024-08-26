bf2295e503a1f7e222e29ea3c6f8b415944ff369
Wichert Akkerman <wichert.akkerman@woven-planet.global>
2024-08-26 11:29:02 +0200

    Add docs rust-zserio and zserio-rs-build
    The main documentation is in the zserio crate. The other crates
    do deserve some minimal content to describe their purpose, and
    link to the zserio crate.

--- crates/zserio-rs-build/src/lib.rs
+++ crates/zserio-rs-build/src/lib.rs
@@ -1,3 +1,15 @@
+//! This package provides a compiler to Generate rust code for the [zserio](http://zserio.org/)
+//! serialization formst. `zserio` is a binary serialization language, similar to Protobuf. The key
+//! features are:
+//!
+//! - It features a rich schema.
+//! - Programming language agnostic.
+//! - Compact and easy to use.
+//! - Good and fast out-of the box compression.
+//!
+//! The syntax is similar to C/C++, which makes zserio code easy to read.
+//!
+//! For more information on zserio in Rust please see [zserio](https://crates.io/crates/zserio).
 mod internal;
 
 pub use internal::generator::model::generate_model;
