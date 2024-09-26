9326413881dcd8390e439133b439e0f3ccee0d61
Wichert Akkerman <wichert@wiggy.net>
2024-09-26 21:30:42 +0200

    Fix rust-zserio backwards compat cli
    This was still using an older name for zserio-rs-build.

--- crates/rust-zserio/src/main.rs
+++ crates/rust-zserio/src/main.rs
@@ -10,13 +10,13 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if ProcessBuilder::new("zserio-cli")
+    if ProcessBuilder::new("zserio-rs-build")
         .args_replace(&args)
         .exec_replace()
         .is_err()
     {
-        eprintln!("Can not run zserio-cli. Please make sure you have installed it.");
+        eprintln!("Can not run zserio-rs-build. Please make sure you have installed it.");
         eprintln!();
-        eprintln!("    cargo install zserio-cli");
+        eprintln!("    cargo install zserio-rs-build");
     }
 }
