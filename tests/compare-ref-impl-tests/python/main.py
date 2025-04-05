51632cbbc65baa8a40241d8710aadfbdcb9cb976
Wichert Akkerman <wichert@wiggy.net>
2025-04-05 21:57:49 +0200

    Use zserio 2.16 on Python 3.13
    We want to support recent Python versions, which requires updating to a
    more recent zserio as well. This changeset switches to the latest
    releases of both Python and zserio. The next step is fixing main.py
    which is now failing due to internal changes in zserio.

--- tests/compare-ref-impl-tests/python/main.py
+++ tests/compare-ref-impl-tests/python/main.py
@@ -1,7 +1,7 @@
 # /// script
-# requires-python = ">=3.11,<3.12"
+# requires-python = ">=3.11"
 # dependencies = [
-#     "zserio~=2.11.0",
+#     "zserio~=2.16.0",
 # ]
 # ///
 from packed_arrays_test import test_packed_arrays
--- tests/gen_reference_modules.sh
+++ tests/gen_reference_modules.sh
@@ -1,2 +1,5 @@
 # Use this to generate the zserio bindings for python and C++ using the zserio reference implementation
-zserio reference_modules/all.zs -setTopLevelPackage pygen -python ./compare-ref-impl-tests/python -cpp ./compare-ref-impl-tests/cppgen
+zserio reference_modules/all.zs \
+    -setTopLevelPackage pygen \
+    -python ./compare-ref-impl-tests/python \
+    -cpp ./compare-ref-impl-tests/cppgen
