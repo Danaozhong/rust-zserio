84ff2ef6e30fde78f6556f745a6828534e8168f6
Clemens Zangl <clemens.zangl@woven-planet.global>
2026-02-16 13:35:35 +0900

    Fix linter Errors
    - Remove helper function, replaced by `x.abs_diff()`.
    - For a unit test that just checks if generated code compiles,
      allow unused variables and assignment.

--- tests/round-trip-tests/src/ambiguous_types_test.rs
+++ tests/round-trip-tests/src/ambiguous_types_test.rs
@@ -1,5 +1,6 @@
 use reference_module_lib::reference_modules::ambiguous_types::main::AmbiguousTypesStruct;
 
+#[allow(unused_assignments, unused_variables)]
 pub fn test_ambiguous_types() {
     // Create a test structure, and assign a new instance.
     // If this line compiles, the test passes.
